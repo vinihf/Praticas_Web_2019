340c2a1bc64067b674c6bd3f4cda75613cd113da
Vinicius H. Ferreira <vinihf@gmail.com>
2019-06-04 15:56:13 -0300

    cors

--- submitForm/main.js
+++ submitForm/main.js
@@ -1,11 +1,12 @@
 const form = document.querySelector('.formulario');
 form.addEventListener("submit",function(e){
     e.preventDefault();
-    const content = new FormData(this);
-    fetch("processa.php", {
-        method: "POST",
-        body: content
+    const value = this.n.value;
+    fetch("https://cors-anywhere.herokuapp.com/https://testandoapi.herokuapp.com/dobro/"+value, {
+    method: "GET",
     })
     .then(response => response.json())
-    .then(data => console.log("OK"+data.mensagem))
+    .then(data => {
+        console.log(data.result);
+    })
 })
