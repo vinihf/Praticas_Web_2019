e5eb29793300fca0887cf08deda01af985186238
Vinicius H. Ferreira <vinihf@gmail.com>
2019-06-18 20:29:33 -0300

    Update main.js

--- submitForm/main.js
+++ submitForm/main.js
@@ -3,7 +3,7 @@ form.addEventListener("submit",function(e){
     e.preventDefault();
     const value = this.n.value;
     fetch("https://cors-anywhere.herokuapp.com/https://testandoapi.herokuapp.com/dobro/"+value, {
-    method: "GET",
+        method: "GET",
     })
     .then(response => response.json())
     .then(data => {
